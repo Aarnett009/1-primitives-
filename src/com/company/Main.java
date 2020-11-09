1a41e9f3339654cb504daae4726c9f76f4bb2a61
aarnett009 <snaplol@hotmail.com>
2020-11-09 02:00:46 -0500

    Added notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         // Prints my name (Every Java program starts with the main function.)
+//        Each time code is edited, use git hub in command line > Stash > Master > Push **Always go into detail
+//        About what it is that you corrected in the code. If you feel the code is better/right, commit push.
         /*Line One
-        * Line Two */
+         * Line Two */
         System.out.println("My name is Andrew Arnett");
     }
 
