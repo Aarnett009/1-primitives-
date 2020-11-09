c7751f4020b15c57f36aa938e65b1725e397bb3c
aarnett009 <snaplol@hotmail.com>
2020-11-09 01:39:41 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // Prints my name (Every Java program starts with the main function.)
+        /*Line One
+        * Line Two */
+        System.out.println("My names is Big Dick Rick");
+    }
+
+}
